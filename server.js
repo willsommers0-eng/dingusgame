00bfc4a68a6e77e32101a5a007b1d59b7bbeb5f4
willsommers0-eng <willsommers0@gmail.com>
2026-07-03 23:55:15 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,8 @@
+{
+  "name": "arena-pvp",
+  "version": "1.0.0",
+  "description": "Browser FPS deathmatch with authoritative room server",
+  "main": "server.js",
+  "scripts": { "start": "node server.js" },
+  "dependencies": { "ws": "^8.18.0" }
+}
